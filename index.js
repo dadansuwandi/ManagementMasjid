54c136f5dbe0a9cf8ea501e1f781b1591fac8777
dadansuwandi <dadan.suwandi83@gmail.com>
2022-08-05 16:41:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const electron = require('electron')
+const {BrowserWindow,app} = electron
+
+
+
+let mainWindow
+mainWin = () => {
+    mainWindow = new BrowserWindow(
+        {
+         webPreferences :{
+            nodeIntegration:true
+         },
+         width:1000,
+         heigth:900,
+         autoHideMenuBar:true
+        })
+    mainWindow.loadFile('index.Html')
+}
+
+app.on('ready',() =>{
+    mainWin()
+    
+   
+})
